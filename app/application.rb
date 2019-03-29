2c7d9fac6d03e73d93ef4c8bac4d97d13a910034
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-29 20:29:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,9 @@ class Application
       item_name = req.path.split("/items/").last
       if item =@@items.find{|i| i.name == item_name}
         resp.write item.price
+        else 
+        resp.status = 400
+        resp.write "Item not found"
       end
     end
     resp.finish
