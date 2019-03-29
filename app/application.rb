a9393d75e4f8149de51da53c9927a86dd8d0a85a
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-29 20:26:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,13 @@ class Application
   
   @@items = [Item.new("Apples", 0.50)]
   
-  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item =@@items.find{|i| i.name == item_name}
+        resp.write item.price
   
 end
